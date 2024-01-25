1d8a84fd52b2bff381f10d4a7b548b2f7d0b8ac7
Ibenye Jehoshaphat <jehoibenye@gmail.com>
2024-01-25 10:23:18 -0800

    hhhdk

---
+++ src/MarvelData.js
@@ -0,0 +1,99 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Riri Williams",
+    alias: "John Wesley",
+    abilities: ["Holiness", "Evangelist"],
+    team: " (1703-1791)",
+    img1: "john welsey.jpeg",
+    homeworld: "England",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+  {
+    name: "Tony Stark",
+    alias: "Pastor kumuyi",
+    img1: "Pastor kumuyi.webp",
+    homeworld: "Gbagada",
+    abilities: [
+      "Holiness",
+      "Healings",
+      "GCK",
+    ],
+    team: "DCLM",
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "Miles Morales",
+    alias: "Charles Wesley",
+    abilities: ["Hymns writers", " (1707-1788)"],
+    team: "Methodist ",
+    img1: "charles wesley.jpeg",
+    homeworld: "England",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+
+
+  {
+    name: "Cindy Moon",
+    alias: "John G Lake",
+    abilities: [" 1870 to 1935", "Healing" ,"Azusa street Revival"],
+    team: "Apostolic Faith Mission ",
+    img1: "john g lake.jpeg",
+    homeworld: "South Africa",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  // Spider-Man
+ 
+  {
+    name: "Pepper Potts",
+    alias: " Pastor Adeboye",
+    abilities: ["Praise   (Shout Hallelulah)", "Miracle"],
+    team: " (RCCG)",
+    img1: "pastor E A Adeboye.jpeg",
+    homeworld: "Redemption Camp",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  
+  {
+    name: "James 'Rhodey' Rhodes",
+    alias: " David Oyedepo",
+    abilities: ["", "Prosperity Gospel"],
+    team: "Winners Chapel",
+    img1: "OYEDEPO.jpeg",
+    homeworld: "Ogun State",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },  
+  {
+    name: "Steve Rogers",
+    alias: "Charles G Finney",
+    abilities: ["August 29,1792–August 16,1875", "Personal Salvation"],
+    team: " Presbyterians",
+    img1: "Charles Finney.jpeg",
+    homeworld: "America",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "Sam Wilson",
+    alias: "Evangelist Jerry A Ifeanyi",
+    abilities: ["Holiness", "Salvation",],
+    team: "Refiner's Fire ",
+    img1: "Austin Jerry.png",
+    homeworld: "Nigeria",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
